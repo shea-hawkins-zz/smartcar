40d2d87faacd32a8e08acad6e8ee4df6e1d394d5
Shea Hawkins <Shea Hawkins>
2016-09-17 16:54:35 -0700

    Standardize GM vehicle info

--- gm/resources/controllers/rootController.js
+++ gm/resources/controllers/rootController.js
@@ -3,10 +3,27 @@ const {
     getVehicleInfo
 } = require('../gets/gets.js');
 
+const standardizeVehicleInfo = function(info) {
+    let doorCount;
+    if (info.data.fourDoorSedan.value.toLowerCase() === 'true') {
+        doorCount = 4;
+        // Currently the only possibilities are 4 door or two door.
+    } else {
+        doorCount = 2;
+    }
+
+    return  {
+        "vin": info.data.vin.value,
+        "color": info.data.color.value,
+        "doorCount": doorCount,
+        "driveTrain": info.data.driveTrain.value
+    };
+};
+
 module.exports = function(req, res) {
     getVehicleInfo(req.params.id)
         .then(info => {
-            res.send(info);
+            res.send(standardizeVehicleInfo(info));
         })
         .catch(err => {
             res.status(400).send(err);
