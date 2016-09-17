aca83d8bb2d5f55d79dacae1465022dcde314fea
Shea Hawkins <Shea Hawkins>
2016-09-17 13:29:47 -0700

    Add developer server logging

--- gm/server.js
+++ gm/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-const port = 3000;
+// The translator service is not exposed to the host machine, and therefore
+// no development conflicts with port 80 are possible.
+const port = 80;
 
-app.listen(3000, (err) => {
+process.env.HOST_ENV === 'prod' ? app.use(morgan('prod')) : app.use(morgan('dev'));
+
+app.listen(port, (err) => {
     if (err) {
         throw err;
     }
